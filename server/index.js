5cb9acde05233b70d9d512851b55ab7ccc5954fb
Mariana Martinez Celis <marianamtzcelis19@gmail.com>
2023-07-01 12:41:06 -0600

    add endpoint to fetch app reviews

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 // this file is used to start the server
-
-const express = require("express");
+import fetch from 'node-fetch'
+import express from 'express'
+import fs from 'fs';
 
 const PORT = process.env.PORT || 3001;
 
@@ -8,6 +9,60 @@ const app = express();
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
+});
+
+// endpoint to get an app´s review list
+app.get("/api/getAppReviews", (req, res) => {
+    const appId = req.query.appId;
+    const filePath = `data/${appId}.json`;
+    const fileExists = fs.existsSync(filePath);
+    let shouldFetchData = true;
+
+    // define if data is recent enough to reuse or fetch again
+    if (fileExists) {
+        const { birthtime } = fs.statSync(filePath);
+        shouldFetchData = (Date.now() - birthtime) > 900000; // 900000 equals 15 minutes
+    }
+
+    // if endpoint was called more than 15 minutes ago, return newly fetched reviews
+    if (shouldFetchData) {
+        fetch(`https://itunes.apple.com/us/rss/customerreviews/id=${appId}/sortBy=mostRecent/page=1/json`, {
+            method: "GET"
+        })
+        .then((res) => res.json())
+        .then((json) => { 
+            const entries = json.feed.entry;
+
+            let entriesList = [];
+            for (const index in entries) {
+                const entry = entries[index];
+                let entryDictionary = {
+                    "reviewTitle": entry.title.label,
+                    "reviewContent": entry.content.label,
+                    "author": entry.author.name.label,
+                    "rating": entry["im:rating"].label,
+                    "date": entry.updated.label
+                };
+                entriesList.push(entryDictionary);
+            }
+
+            const jsonString = JSON.stringify(entriesList);
+            fs.writeFileSync(`data/${appId}.json`, jsonString);
+            res.json({ message: JSON.parse(jsonString) });
+        })
+
+    } else {
+
+        // if endpoint was called less than 15 minutes ago, return cached reviews
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            res.json({ message: JSON.parse(data) });
+          });
+    }
+
 });
 
 app.listen(PORT, () => {
