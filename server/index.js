96e8eaaec55c4ab3be7a9a482e792b52c5d64b0f
Mariana Martinez Celis <marianamtzcelis19@gmail.com>
2023-07-01 10:38:12 -0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,15 @@
+// this file is used to start the server
+
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
