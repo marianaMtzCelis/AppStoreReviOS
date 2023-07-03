e8587473a272ef90643c20430e4f8118c0091fd3
Mariana Martinez Celis <marianamtzcelis19@gmail.com>
2023-07-02 18:26:53 -0600

    setup component routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,21 @@
-// client/src/App.js
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import AboutMe from './components/AboutMe';
+import Reviews from './components/Reviews';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+   function App() {
+    return (
+      <ThemeProvider theme={createTheme()}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/aboutMe" element={<AboutMe />} />
+          <Route path="/reviews" element={<Reviews />} />
+        </Routes>
+      </Router>
+      </ThemeProvider>
+    );
+   }
 
-function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+   export default App;
